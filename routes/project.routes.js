e81ab9564f7d3786a2538dd110a4e26716574fe8
Diogo Guimaraes <mail@diogoguimaraes.pt>
2023-03-09 11:51:57 +0000

    delete working --- update not updating

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -2,7 +2,6 @@ const Project = require("../models/Project.model");
 const uploader = require('../middleware/cloudinary.config');
 const express = require("express");
 const router = express.Router();
-
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
@@ -24,26 +23,23 @@ router.get('/', async (req, res) => {
 router.post('/new', uploader.single("imageUrl"), async (req, res, next) => {
   if (!req.file) {
     res.status(200).json({ message: "no image" });
-  } else {    
-    //get the image url in 'req.file.path'   
-    // res.status(200).json({images: req.file.path});
-    console.log(req.file.path);
-  try {
-    const { name, description } = req.body;
-     
-    const newProject = await Project.create({
-      name: req.body.name,
-      images: [req.file.path],
-      description: req.body.description,
-      // owner:req.session.user, DECODE TOKEN - GET USER ID --- SEND TO PROJECT
-    });
-    console.log(newProject);
-    res.status(201).json({ message: 'Project created', project: newProject });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
+  } else {
+    try {
+      const { name, description } = req.body;
+      const newProject = await Project.create({
+        name: req.body.name,
+        images: [req.file.path],
+        description: req.body.description,
+        // owner:req.session.user, DECODE TOKEN - GET USER ID --- SEND TO PROJECT
+      });
+      console.log(newProject);
+      res.status(201).json({ message: 'Project created', project: newProject });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server error');
+    }
   }
-}});
+});
 // Get a specific project by ID
 router.get('/:projectId', async (req, res) => {
   try {
@@ -51,7 +47,7 @@ router.get('/:projectId', async (req, res) => {
     if (!project) {
       res.status(404).send('Project not found');
     } else {
-      res.send(201).json({ message: 'Project found', project: project._id })
+      res.status(201).json({ message: 'Project found', project });
     }
   } catch (err) {
     console.error(err);
@@ -59,18 +55,32 @@ router.get('/:projectId', async (req, res) => {
   }
 });
 // Update a specific project by ID
-router.post('/:projectId/update', async (req, res) => {
+router.get('/updated/:projectId', async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.projectId);
+    if (!project) {
+      res.status(404).send('Project not found');
+    } else {
+      console.log(project)
+      res.status(201).json({ message: 'Project found', project });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+router.put('/:projectId/update', async (req, res) => {
   try {
     const { name, image, description } = req.body;
     const updatedProject = await Project.findByIdAndUpdate(
-      req.params.ProjectId,
+      req.params.projectId,
       { name, image, description },
       { new: true }
     );
     if (!updatedProject) {
       res.status(404).send('Project not found');
     } else {
-      res.redirect(`/projects/${req.params.projectId}`);
+      res.status(201).json({ message: 'Project updated', project: updatedProject });
     }
   } catch (err) {
     console.error(err);
@@ -78,13 +88,13 @@ router.post('/:projectId/update', async (req, res) => {
   }
 });
 // Delete a specific project by ID
-router.post('/:projectId/delete', async (req, res) => {
+router.delete('/:projectId', async (req, res) => {
   try {
     const deletedProject = await Project.findByIdAndDelete(req.params.projectId);
     if (!deletedProject) {
       res.status(404).send('Project not found');
     } else {
-      res.redirect('/projects');
+      res.status(200).json({ message: 'Project deleted', project: deletedProject });
     }
   } catch (err) {
     console.error(err);
